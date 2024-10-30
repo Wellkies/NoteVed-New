56247342fc7b6307cd4b657e794828c340260d78
Mitali <mitalimnoteved@gmail.com>
2024-10-30 15:48:36 +0530

    image question changes

--- src/screens/UserScreen/MockTests.tsx
+++ src/screens/UserScreen/MockTests.tsx
@@ -913,8 +913,8 @@ const MockTests = ({route}) => {
                           }}>
                           <View
                             style={{
-                              width: 60,
-                              height: 60,
+                              width: IsTabScreen ? 70 : 60,
+                              height: IsTabScreen ? 70 : 60,
                               borderRadius: 10,
                               overflow: 'hidden',
                             }}>
